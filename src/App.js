8f3fc587a55ce086b51da5001278f899db249048
Renard Lomax <renardlomax1990@gmail.com>
2021-05-10 10:09:48 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const ATMDeposit = ({ onChange, isDeposit, isValid}) => {
+    const choice = ['Deposit', 'Cash Back', 'Clear'];
+    console.log(`ATM isDeposit: ${isDeposit}`);
+    console.log('isValid', isValid)
+    return (
+      <label className="label huge">
+        <h3 > {choice[Number(!isDeposit)]}</h3>
+        $ <input  placeholder="Enter Amount" id="number-input" type="number" width="200" onChange= {onChange}></input>
+        <input type="submit" disabled={!isValid}  width="200" value="Submit" id="submit-input"></input>
+      </label>
+    );
+  };
+  
+  const Account = () => {
+    const [deposit, setDeposit] = React.useState(0);
+    const [totalState, setTotalState] = React.useState(0);
+    const [isDeposit, setIsDeposit] = React.useState(true);
+    const [atmMode, setAtmMode] = React.useState("");
+    const [validTransaction, setValidTransaction] = React.useState(false);
+ 
+    let status = `Account Balance $ ${totalState} `;
+    console.log(`Account Rendered with isDeposit: ${isDeposit}`);
+    const handleChange = (event) => {
+      console.log(`handleChange ${event.target.value}`);
+      console.log('type',typeof(event.target.value))
+      if(Number(event.target.value) <= 0){
+        return setValidTransaction(false);
+      }
+      if(atmMode === "Cash Back" && (Number(event.target.value) > totalState)){
+        setValidTransaction(false)
+      }else{
+        setValidTransaction(true)
+      }
+      
+      setDeposit(Number(event.target.value));
+    };
+    
+    const handleSubmit = (event) => {
+      let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+      setTotalState(newTotal);
+      setValidTransaction(false);
+      event.preventDefault();
+    };
+    const handleModeSelect = (event) => {
+      setValidTransaction(false);
+      setAtmMode(event.target.value);
+      if(event.target.value === "Deposit"){
+        setIsDeposit(true);
+      }else{
+        setIsDeposit(false);
+      }
+    }
+    return ( 
+      <div>
+        
+ <form onSubmit={handleSubmit}>
+   
+        <h2 id="total">{status}</h2>
+        <label class= "labels">Select Opperation</label>
+        <select  class= "tab" onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
+        <option id="no-selection" value="" ></option>
+        <option id="deposit-selection" value="Deposit">Deposit</option>
+        <option id="cashback-selection" value="Cash Back">Cash Back</option>
+        </select>
+        { !(atmMode === "") &&
+        <ATMDeposit onChange={handleChange} isDeposit={isDeposit} isValid={validTransaction}></ATMDeposit>
+        }
+      </form>
     </div>
-  );
-}
-
-export default App;
+    );
+  };
+  
+ export default Account
